a43b15e08e6f9fc38fc9191abd29ab4e6c7282af
Etativel <farhanmaulana4611@gmail.com>
2024-10-15 22:20:58 +0800

    add prettyPrint function to visualize tree

--- script.js
+++ script.js
@@ -19,10 +19,31 @@ class Tree {
 
     root.left = this.buildTree(array.slice(0, mid));
     root.right = this.buildTree(array.slice(mid + 1));
-
+    this.tree = root;
     return root;
+  }
+
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 }
 
-const tree = new Tree();
-console.log(tree.buildTree([1, 2, 4, 45, 3, 23]));
+const tree = new Tree([
+  1, 2, 4, 45, 3, 23, 45, 345, 213, 5, 6423, 4, 43, 4453, 45, 343243, 345345,
+  243, 5, 456, 45, 43, 6, 46, 4, 6, 456, 4, 654, 6, 456, 45, 64, 7, 64213, 4245,
+  756,
+]);
+tree.prettyPrint();
