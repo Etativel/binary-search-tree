6ef41c09c97704eaba70dfd19733dc973915f34d
Etativel <farhanmaulana4611@gmail.com>
2024-10-16 10:55:18 +0800

    add deleteItem function to delete the given value from the tree

--- script.js
+++ script.js
@@ -53,8 +53,35 @@ class Tree {
   }
 
   deleteItem(value, root = this.root) {
-    return;
+    if (root === null) return root;
+    if (value < root.data) {
+      root.left = this.deleteItem(value, root.left);
+    } else if (value > root.data) {
+      root.right = this.deleteItem(value, root.right);
+    } else {
+      // the value is equal to the root data
+      // Case 1: Node with no child
+      if (root.left === null) return root.right;
+      if (root.right === null) return root.left;
+
+      // Case 2: Node with two children
+      root.data = this.findMinValue(root.right).data;
+      // Find the smallest node in the right subtree (which is guaranteed to be larger than the node to be deleted but smaller than all other nodes in the right subtree).
+      // Replace the node's data with this smallest node's data.
+      // Delete the smallest node from the right subtree to avoid duplicates.
+      root.right = this.deleteItem(root.data, root.right);
+    }
+    return root;
   }
+
+  findMinValue(root) {
+    let current = root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current;
+  }
+
   find(value) {}
 
   levelOrder(callback) {}
@@ -72,6 +99,11 @@ const tree = new Tree([1, 2, 4, 45]);
 //
 
 tree.insert(234);
-tree.insert(234);
-tree.insert(234);
+tree.insert(237);
+tree.insert(236);
+tree.insert(232);
+tree.insert(233);
+tree.insert(44);
+tree.prettyPrint();
+tree.deleteItem(234);
 tree.prettyPrint();
